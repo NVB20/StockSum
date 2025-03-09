f36d8f104906df3d3b11390dcfe928d3d5a34fb2
Niv <nivb49@gmail.com>
2025-03-09 19:12:02 +0200

    docker ignore

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from tests.validations import check_values
 from handle.result import result_calc
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.secret_key = 'your_secret_key'
     
 
 @app.route('/', methods=['GET', 'POST'])
