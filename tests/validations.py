431f5e9c6b3d62ddf8488e0bd20e17a474ea4b6c
Niv <nivb49@gmail.com>
2025-03-22 20:33:08 +0200

    validations

--- tests/validations.py
+++ tests/validations.py
@@ -5,6 +5,13 @@ def check_values(high, low, risk):
         return "Low must be a Number"
     if not str(risk).isdigit():
         return "Risk must be a Number"
+    
+    
+    high = int(high)
+    low = int(low)
+    risk = int(risk)
+
+
     if risk <= 0: 
         return "Risk must be greater than 0"
     if high <= 0: 
