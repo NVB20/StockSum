8949636f5cf6026396f643fdc3c0b2d97411321d
Niv <nivb49@gmail.com>
2025-03-23 10:41:24 +0200

    better validation

--- handle/validations.py
+++ handle/validations.py
@@ -1,23 +1,16 @@
 def check_values(high, low, risk):
-    if not str(high).isdigit():
-        return "High must be a Number"
-    if not str(low).isdigit():
-        return "Low must be a Number"
-    if not str(risk).isdigit():
-        return "Risk must be a Number"
+    try:
+        high, low, risk = int(high), int(low), int(risk)
+    except ValueError:
+        return "All inputs must be numbers"
     
-    
-    high = int(high)
-    low = int(low)
-    risk = int(risk)
-
-
-    if risk <= 0: 
-        return "Risk must be greater than 0"
-    if high <= 0: 
+    if high <= 0:
         return "High must be greater than 0"
-    if low <= 0: 
+    if low <= 0:
         return "Low must be greater than 0"
+    if risk <= 0:
+        return "Risk must be greater than 0"
     if low >= high:
-        return "Low cant be bigger than High"
+        return "Low can't be bigger than High"
+    
     return None
