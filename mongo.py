823f13b3e2889cb69e74af2bfeb15cf6372fc73a
Niv <nivb49@gmail.com>
2025-03-30 22:46:42 +0300

    add ttl to mongo

--- mongo.py
+++ mongo.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import ASCENDING, MongoClient
 import datetime
 
 
@@ -6,15 +6,17 @@ MONGO_URI = "mongodb://localhost:27017/"
 client = MongoClient(MONGO_URI, username='admin', password='adminpass')
 db = client["results"]
 collection = db["res1"]
+collection.create_index([("createdAt", ASCENDING)], expireAfterSeconds=3600)
+
 
 
 def insert_res(result, username):
     date_time = datetime.datetime.now()
-    result_save = {"username": username,  "date": date_time, "result": result}
+    result_save = {"username": username,  "createdAt": date_time, "result": result}
     collection.insert_one(result_save)
 
 def get_history(username):
-    results = collection.find({"username": username}).sort("date", 1)  # Sort by date in ascending order
+    results = collection.find({"username": username}).sort("createdAt", 1)  # Sort by date in ascending order
     history = list(results)  # Convert cursor to list
     return history
  
